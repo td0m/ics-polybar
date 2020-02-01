6fd5fff79efd5ba0bded6ef48337bfa816b59173
d0minikt <dominik.t.uk@gmail.com>
2020-02-01 13:19:36 +0000

    Improve date formatting
    Rewrite formatting by using date difference and take into account larger
    date differences.

--- main.go
+++ main.go
@@ -3,62 +3,65 @@ package main
 import (
 	"fmt"
 	"github.com/apognu/gocal"
+	"math"
 	"os"
 	"strings"
 	"time"
 )
 
-func prettyTime(now time.Time, date *time.Time) string {
-	dayDiff := date.Day() - now.Day()
-	if dayDiff > 0 {
-		return fmt.Sprintf("at %d:%02d", date.Hour(), date.Minute())
-	}
-	hourDiff := date.Hour() - now.Hour()
-	if hourDiff <= 1 {
-		minuteDiff := 60*hourDiff + date.Minute() - now.Minute()
+// returns a formatted date string
+func formatDate(date time.Time) string {
+	now := time.Now()
+	diff := date.Sub(now)
+	minuteDiff := int(math.Floor(diff.Minutes())) % 60
+	hourDiff := int(math.Floor(diff.Hours()))
+	fmt.Println(now.Format("Mon 2 Jan at 15:04"))
+
+	if diff.Minutes() <= 60 {
 		return fmt.Sprintf("in %d minutes", minuteDiff)
-
 	}
-	return fmt.Sprintf("in %d hours", hourDiff)
+	if diff.Hours() <= 12 {
+		return fmt.Sprintf("in %dh %d min", hourDiff, minuteDiff)
+	}
+	formattedHour := date.Format("15:04")
+	if now.YearDay() == date.YearDay() {
+		return fmt.Sprintf("today at %s", formattedHour)
+	}
+	if now.YearDay()+1 == date.YearDay() {
+		return fmt.Sprintf("tomorrow at %s", formattedHour)
+	}
+	if now.YearDay()+7 >= date.YearDay() {
+		return date.Format("Mon at 15:04")
+	}
+
+	return date.Format("Mon 2 Jan at 15:04")
 }
 
-func prettyDay(now time.Time, date *time.Time) string {
-	dayDiff := date.Day() - now.Day()
-	if dayDiff == 0 {
-		return ""
-	}
-	if dayDiff == 1 {
-		return "tomorrow"
-	}
-	return fmt.Sprintf("in %d days", now.Day())
-
-}
-
-func prettyDate(now time.Time, date *time.Time) string {
-	return fmt.Sprintf("%s %s", prettyDay(now, date), prettyTime(now, date))
-}
-
-func prettyName(summary string) string {
+// returns a formatted name (you might have to change this)
+// my events follow a "{MODULE_CODE} - {MODULE_NAME}" convention
+// returns whole string if convention not recognised
+func formatName(summary string) string {
 	parts := strings.Split(summary, "-")
-	if len(parts) == 1 {
+	if len(parts) != 2 {
 		return summary
 	}
 	return strings.TrimSpace(parts[1])
 }
 
 func main() {
+	// open the calendar file
 	f, _ := os.Open("./calendar.ics")
 	defer f.Close()
 
 	start, end := time.Now(), time.Now().Add(12*30*24*time.Hour)
 
+	// parse the ics file
 	c := gocal.NewParser(f)
 	c.Start, c.End = &start, &end
 	c.Parse()
 
+	// get the next event in the calendar
 	now := time.Now()
-	// now = now.Add(time.Hour * 24)
-
 	var next gocal.Event
 	for _, e := range c.Events {
 		// fmt.Printf("%s on %s by\n", e.Summary, e.Start)
@@ -67,5 +70,8 @@ func main() {
 			break
 		}
 	}
-	fmt.Printf("%s, %s\n", prettyName(next.Summary), prettyDate(now, next.Start))
+	// output the formatted output to stdout
+	// if you would like to add a room, do it here
+	// most of my lectures are in the same theathre anyway, so I don't need to waste space on a room number
+	fmt.Printf("%s, %s\n", formatName(next.Summary), formatDate(*next.Start))
 }
